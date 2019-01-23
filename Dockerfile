a459daa9c27e1d7ed035ca79d60e7f3116bd6209
Tito Villalobos <tito.villalobos.1@gmail.com>
2019-01-23 13:52:22 -0500

    Updated image to load files from a mapped vault/init/

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM vault:0.10.0
+FROM vault:1.0.2
 
-RUN apk add --update python
+# Our entrypoint uses a backround server, which doesn't use the vault server "-dev" flag
+# Therefore we need a basic config for the server to start up.
+COPY ./vault/config/ /vault/config
 
-ADD ./vault/ /vault
-WORKDIR /vault
+# NOTE: this copies to cci-docker-entrypoint.sh
+# Copy the new entrypoint sh along side the one from the base docker image.
+# This is because ours calls into the base entrypoint.sh from hashicorp to get things like IPC_LOCK checking.
+COPY ./vault/docker-entrypoint.sh /usr/local/bin/cci-docker-entrypoint.sh
 
 ENV VAULT_ADDR=http://127.0.0.1:8200
 
-EXPOSE 8201
-
-ENTRYPOINT ["./docker-entrypoint.sh"]
+ENTRYPOINT ["/usr/local/bin/cci-docker-entrypoint.sh"]
--- vault/docker-entrypoint.sh
+++ vault/docker-entrypoint.sh
@@ -1,80 +1,129 @@
-#!/bin/sh
-set -ex
+#!/usr/bin/dumb-init /bin/sh
+# Use the same dumb-init process that the base hashcorp docker-entrypoint.sh uses.
+# This init system helps ensure zombie processes are reaped and subprocess cleanup from vault happens correctly.
+# See their comment: https://github.com/hashicorp/docker-vault/blob/0.11.0-beta1/0.X/docker-entrypoint.sh#L1-L7
 
-mkdir -p /vault/__restricted;
+set -e
 
-status() {
+# Overview:
+# 1) Starts up the vault server using the base script from hashicorp.
+# 2) Waits for the vault server then connects and initializes the vault
+#     a) inits the vault and sets up the unseal key and root token
+#     b) looks at the mounted /vault/init/*.sh (if any) and runs those to setup data
+#           (secrets engines, kv secrets, transit keys, policies, tokens, etc)
+
+# Run vault using the hashicorp docker-entrypoint.sh
+# This gives us chown and IPC_LOCK validation
+vault_server() {
+  /usr/local/bin/docker-entrypoint.sh server
+}
+
+# ensure all directories are created, for any that haven't been mounted
+init_directories() {
+  echo "** Ensuring directories exit"
+  # directory for init scripts
+  mkdir -p /vault/init/
+  # directory for the unseal key and root tokens
+  mkdir -p /vault/root/
+  # directory for output tokens from the scripts
+  mkdir -p /vault/tokens/
+}
+
+vault_status() {
   vault status 2>&1
 }
 
-wait() {
+wait_for_vault() {
   NUM_TRIES=0
   while [ $NUM_TRIES -lt 5 ]; do
-    if status | grep -v 'connection refused'; then
+    if vault_status | grep -v 'connection refused'; then
       NUM_TRIES=5
-      echo vault ready;
+      echo "vault ready";
     else
       NUM_TRIES=$((NUM_TRIES + 1))
       sleep 2
     fi
   done
 
-  if status | grep 'connection refused'; then
+  if vault_status | grep 'connection refused'; then
     exit 27
   fi
 }
 
-setup_token() {
-  root_token=$(grep 'Root' /vault/file/vault-init-out | sed 's/^.*: //g')
+# Initialize the new vault
+init_vault() {
+  echo "** Initializing Vault"
+  # Configure one key to unseal. This is a dev/test vault, so the unseal doesn't need to be m-of-n.
+  vault operator init \
+    -key-shares=1 \
+    -key-threshold=1 \
+    > /vault/root/init.txt 2>&1
 
-  vault login "$root_token"
-
-  # period of 32 days which is the maximum permitted
-  vault token create -period="768h" > /vault/file/client-token;
-  grep -e 'token ' /vault/file/client-token | sed 's/^token\W*//g' > /vault/__restricted/client-token;
+  grep 'Unseal' /vault/root/init.txt | sed 's/^.*: //g' | head -n 1 > /vault/root/unseal_key.txt
+  grep 'Root' /vault/root/init.txt | sed 's/^.*: //g' > /vault/root/root_token.txt
 }
 
-unseal() {
-  keys=$(head -n 3 /vault/file/vault-init-out | sed 's/^.*: //g')
-
-  for k in $keys; do
-    vault operator unseal "$k";
-  done;
-
-  setup_token;
+unseal_vault() {
+  echo "** Unsealing Vault"
+  local unseal_key=$(cat /vault/root/unseal_key.txt)
+  vault operator unseal "$unseal_key"
 }
 
-bootstrap() {
-  vault operator init > /vault/file/vault-init-out 2>&1
+login_as_root() {
+  echo "** Logging Into Vault"
+  # Login to the new vault using the root key for the remaining operations
+  local root_token=$(cat /vault/root/root_token.txt)
 
-  unseal;
-
-  vault kv enable-versioning secret/
-  vault secrets enable transit
+  # redirect output so we don't log the root key.
+  # its a dev server, but still good practice.
+  # Mount the /vault/root/ to get the unseal key and root token
+  vault login "$root_token" > /dev/null
 }
 
-child_process () {
-  wait;
-  if [ -s /vault/file/vault-init-out ]; then
-    unseal;
+# Load the custom init data script(s)
+run_init_scripts() {
+  echo "** Running Init Scripts"
+  if [ -e "/vault/init" ]; then
+    # change dir so that references to policy.hcl files can be relative to the init path
+    (cd /vault/init
+     for file in *.sh; do
+       echo "Running /vault/init/$file"
+       source "$file"
+     done)
   else
-    bootstrap;
+    echo "  No init scripts found"
   fi
-
-  status;
 }
 
-vault_server() {
-  if [ "skip" = "${SKIP_SETCAP:-}" ]; then
-    sed -i 's/false/true/g' /vault/config/vault.json;
-  fi;
-  vault server -config=/vault/config/;
+setup_vault() {
+  init_directories
+  wait_for_vault
+  init_vault
+  unseal_vault
+  login_as_root
+  run_init_scripts
 }
 
-python_http_server() {
-  # we want to be able to serve the VAULT_TOKEN for testing
-  cd /vault/__restricted;
-  python -m SimpleHTTPServer 8201;
+##################################################
+# These are helper functions for the init scripts
+
+# Parse the standard output of "vault token create" to get just the token
+# Usage: "vault token create [OPTIONS] | parse_vault_token > token_file.txt"
+parse_vault_token() {
+   grep '^token\s' | sed -e 's/^token[[:space:]]*//'
 }
 
-child_process & vault_server & python_http_server
+# Contains the best practices for vault token creation (no "default" policy and one policy)
+# Usage "create_vault_token foo-service-policy > foo_service_token.txt
+create_vault_token() {
+  vault token create -no-default-policy -policy=$1 | parse_vault_token
+}
+##################################################
+
+# Actually run the server & setup.
+# The order here is a bit weird to look at, but it is essential.
+# "setup_vault &" to run it in the background, but "vault_server" is run as the foreground task.
+# The wait_for_vault handles waiting for vault to startup.
+# If we ran vault_server in the background, the script ends and the container dies.
+# We want the container to run as long as the vault server is up.
+setup_vault & vault_server
